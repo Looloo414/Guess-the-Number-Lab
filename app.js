91ef5f0f2647ebc8e96d53a5cc8edc13e61fcc59
Looloo414 <alexafaulkison@gmail.com>
2020-11-08 12:10:20 -0700

    Rough Draft

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
 
-  const game = {
-    title: 'Guess the Number!',
-    biggestNum: 100,
-    smallestNum: 1,
-    secretNum: null,
-    prevGuesses: [],
-    play: function() {
-      this.secretNum = Math.floor(Math.random() * 
-        (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
-    }
-  };
+const game = {
+  title: 'Guess the Number!',
+  biggestNum: 100,
+  smallestNum: 1,
+  secretNum: null,
+  prevGuesses: [],
 
-function getGuess(){
-  
+  play: function() {
+    this.secretNum = Math.floor(Math.random() * (this.biggestNum - this.smallestNum + 1)) + this.smallestNum; // random num 1=100
+
+  let input = NaN
+  while(this.secretNum != input) {
+      input = this.getGuess();
+     this.prevGuesses.push(input);
+      this.render(input);
+      if (input === this.secretNum)
+      return;
+  } 
+  },
+  getGuess: function(){  // check back guess === NaN
+      let input = NaN
+  while(isNaN(input)|| input < this.smallestNum || input > this.biggestNum){
+         input = parseInt(prompt( `Enter a guess between ${this.smallestNum} and ${this.biggestNum}`))
+  } return input;
+
+  },
+
+  render: function(input){ // passed in guess argument
+     let result = input === this.secretNum ? alert(` Congrats! You guessed the number in ${this.prevGuesses.length} guesses!`)  
+       : input < this.secretNum ? alert(`${input} was too low  Previous guesses:${this.prevGuesses.join(', ')}`) 
+       : input > this.secretNum ? alert(`${input} was too high  Previous guesses:${this.prevGuesses.join(', ')}`)
+       : NaN; 
+
+       alert(result)
+  }
 }
-
-
+game.play();
 
 
 
