5e0b7a75b38be6ef7aa4deeb16309a03e17c332a
Looloo414 <alexafaulkison@gmail.com>
2020-11-09 08:51:37 -0700

    Added Bonus

--- app.js
+++ app.js
@@ -1,37 +1,50 @@
 
 const game = {
+  //initialize state
   title: 'Guess the Number!',
-  biggestNum: 100,
-  smallestNum: 1,
+  biggestNum: null,
+  smallestNum: null,
   secretNum: null,
   prevGuesses: [],
 
   play: function() {
+    this.biggestNum = parseInt(prompt(`Enter the top number of your range.`));
+    parseInt(prompt(`Enter the bottom number of your guessing `));
     this.secretNum = Math.floor(Math.random() * (this.biggestNum - this.smallestNum + 1)) + this.smallestNum; // random num 1=100
 
-  let input = NaN
-  while(this.secretNum != input) {
+    let input = NaN
+    while(this.secretNum != input) {
       input = this.getGuess();
-     this.prevGuesses.push(input);
+      this.prevGuesses.push(input);
       this.render(input);
-      if (input === this.secretNum)
-      return;
-  } 
+      if (input === this.secretNum){
+        return;
+      }
+    } 
   },
   getGuess: function(){  // check back guess === NaN
-      let input = NaN
-  while(isNaN(input)|| input < this.smallestNum || input > this.biggestNum){
-         input = parseInt(prompt( `Enter a guess between ${this.smallestNum} and ${this.biggestNum}`))
-  } return input;
-
+    let input = NaN
+    while(isNaN(input)|| input < this.smallestNum || input > this.biggestNum){
+      input = parseInt(prompt( `Enter a guess between ${this.smallestNum} and ${this.biggestNum}`))
+    } return input;
   },
 
-  render: function(input){ // passed in guess argument
-     let result = (input === this.secretNum) ? alert(` Congrats! You guessed the number in ${this.prevGuesses.length} guesses!`)  
-       : input < this.secretNum ? alert(`${input} was too low  Previous guesses:${this.prevGuesses.join(', ')}`) 
-       : input > this.secretNum ? alert(`${input} was too high  Previous guesses:${this.prevGuesses.join(', ')}`)
-       : 
-       alert(result)
+  render: function(input){ 
+    console.log('Guess', input)
+    if(input === this.secretNum){
+      alert(` Congrats! You guessed the number in ${this.prevGuesses.length} guesses!`)
+    } else if(input < this.secretNum){
+        alert(`${input} was too low \r\n Previous guesses:${this.prevGuesses.join(', ')}`)
+      } else{
+        alert( `${input} was too high \r\n Previous guesses:${this.prevGuesses.join(', ')}`)
+      }
+    
+  //   // passed in guess argument
+  //   //  let result = (input === this.secretNum) ? alert(` Congrats! You guessed the number in ${this.prevGuesses.length} guesses!`)  
+  //   //    : input < this.secretNum ? alert(`${input} was too low \r\n Previous guesses:${this.prevGuesses.join(', ')}`) 
+  //   //    : input > this.secretNum ? alert(`${input} was too high \r\n Previous guesses:${this.prevGuesses.join(', ')}`)
+  //   //    : 
+  //   //    alert(result)
   }
 }
 game.play();
